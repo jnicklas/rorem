28bac8c0a8afed9c3e54562c2111495a822f941c
Jonas Nicklas <jonas.nicklas@gmail.com>
2007-12-19 21:11:31 +0100

    smarter matchers

--- lib/matchers/english.rb
+++ lib/matchers/english.rb
@@ -4,21 +4,35 @@ Rorem.add_matcher(/_type$/, :nothing)
 Rorem.add_matcher(/_id$/, :nothing)
 
 # Match names
-Rorem.add_matcher('name', :name, :table => %w(users people contacts admins employees players))
-Rorem.add_matcher(%w(first_name firstname given_name givenname), :first_name)
-Rorem.add_matcher(%w(last_name lastname family_name familyname sur_name surname), :last_name)
+Rorem.add_matcher('name', :table => %w(users people contacts admins employees players)) do |rorem|
+  @first_name ||= rorem.first_name
+  @last_name ||= rorem.last_name
+  [@first_name, @last_name].join(' ')
+end
+
+Rorem.add_matcher(%w(first_name firstname given_name givenname)) do |rorem|
+  @first_name ||= rorem.first_name
+end
+
+Rorem.add_matcher(%w(last_name lastname family_name familyname sur_name surname)) do |rorem|
+  @last_name ||= rorem.last_name
+end
 
 Rorem.add_matcher('email') do |rorem|
   tlds = %w(se com de org net co.uk co.jp)
-  rorem.first_name.downcase << '.' << rorem.last_name.downcase << '@' << rorem.word(6..12) << '.' << tlds.random
+  @domain ||= "#{rorem.word(6..12)}.#{tlds.random}"
+  @first_name ||= rorem.first_name
+  @last_name ||= rorem.last_name
+  "#{@first_name.downcase}.#{@last_name.downcase}@#{@domain}"
 end
 
 Rorem.add_matcher( %w(birthdate date_of_birth dateofbirth) ) {|r| r.date 45.years.ago..18.years.ago }
 
 Rorem.add_matcher('permalink') do |rorem|
-  Array.new(rorem.integer(1..5)) { rorem.word }.join('-')
+  @title = rorem.title
+  @title.downcase.gsub(/[a-z0-9]/, '-')
 end
 
 Rorem.add_matcher(/^computerized_/, :word )
 Rorem.add_matcher(/^crypted_/, :digest)
-Rorem.add_matcher(:salt, :digest)
+Rorem.add_matcher('salt', :digest)
